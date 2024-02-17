3dffb91d3a0a7032750bbb6739d041ad78bf8b7c
jkrejchi <jkrejchi@iastate.edu>
2024-02-17 17:04:49 -0600

    lol

--- newfrontend/src/components/listItems.js
+++ newfrontend/src/components/listItems.js
@@ -1,18 +1,47 @@
-import React from 'react';
-import './listItem.css'
+import React, { useState } from 'react';
+import './listItem.css';
+
 
 class ListItem extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            expand: 'none',
+            extras: 'See More'
+        }
+    }
+
     render() {
+
+    const toggle = () => {
+        if (this.state.expand == 'none'){
+            this.setState({expand: 'block'});
+            this.setState({extras: 'See Less'});
+        }
+        else {
+            this.setState({expand: 'none'});
+            this.setState({extras: 'See More'});
+        }
+    }
+
+    const pdfUrl = 'https://animaldrugsatfda.fda.gov/adafda/app/search/public/document/downloadBBL/'+ this.props.pdf;
+
       return (
         <div className='item-holder'>
             <p className='asdf'>Common Names: {this.props.commonNames}</p>
             <p className='asdf'>Active Ingredients: {this.props.activeIng}</p>
             <div className='asdf' dangerouslySetInnerHTML={{__html:this.props.indicators}}/>
-            <p className='asdf'>PDF: {this.props.pdf}</p>
-            <button>See more</button>
+            <p className='asdf' target="_blank" rel="noopener noreferrer">PDF: <a href={pdfUrl}>See</a></p>
+            <p className='asdf' style={{display: this.state.expand}}>Animal Class: {this.props.animalClass}</p>
+            <p className='asdf' style={{display: this.state.expand}}>Status: {this.props.status}</p>
+            <p className='asdf' style={{display: this.state.expand}}>Application Number: {this.props.applicationNumber}</p>
+            <p className='asdf' style={{display: this.state.expand}}>Label Type: {this.props.labelType}</p>
+            <button onClick={toggle}>{this.state.extras}</button>
         </div>
       );
     }
   }
+
   
 export default ListItem;
